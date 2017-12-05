74ef93e2fb8d414cd43ae694b92bda64aa665c29
tanner heaps <tanner.heaps@gmail.com>
2017-12-04 21:55:32 -0600

    made more changes

--- check-list.js
+++ check-list.js
@@ -1,13 +1,32 @@
-var task = document.querySelector("#add");
-var submitButton = document.querySelector("#button");
-console.log(task);
-console.log(submitButton);
-submitButton.addEventListener("click", function() {
-    console.log('you pressed a button!');
-    console.log(task.value);
+ var task = document.querySelector("#txt-task");
+    var submitButton = document.querySelector("#btn-submit");
 
-    localStorage.todos = task.value;
+    submitButton.addEventListener("click", function() {
+        if (localStorage.taskCount)
+            localStorage.taskCount = Number(localStorage.taskCount) + 1;
+        else localStorage.taskCount = "1";
 
-    document.querySelector("#todos").innerHTML =localStorage.todos;
-});
+        localStorage["task" + localStorage.taskCount] = task.value;
 
+        dispalyTaskFromLocalStorage();
+    });
+
+
+    function dispalyTaskFromLocalStorage() {
+        var tasksToDisplay = "";
+        for (var i = 1; i <= localStorage.taskCount; i++) {
+            if (localStorage["task" + i]) {
+                tasksToDisplay = localStorage["task" + i] + "<i id='btn-delete' onclick='removeTaskFromLocalStorage(" + i + ")' class='fa fa-times-circle' aria-hidden='true'></i>" + "<br />" +
+                    tasksToDisplay;
+            }
+        }
+
+        document.querySelector("#todos").innerHTML = tasksToDisplay;
+    }
+
+    function removeTaskFromLocalStorage(taskNumber) {
+        localStorage.removeItem("task" + taskNumber);
+        dispalyTaskFromLocalStorage();
+
+    }
+    dispalyTaskFromLocalStorage();
